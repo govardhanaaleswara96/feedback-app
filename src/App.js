5d440dc71c5e6f808c0a073a4cd100d6353d9f5a
govardhanaaleswara96 <govardhanaaleswara@gmail.com>
2023-12-12 23:15:45 +0530

    components added

--- src/App.js
+++ src/App.js
@@ -1,31 +1,17 @@
-function App(){
+import { useState } from "react";
+import Header from "./components/Header";
+import FeedbackList from "./components/FeedbackList";
+import FeedbackData from "./data/Feedback";
 
-    const tittle = 'Block Post';
-    const para = 'I am block post component';
-    const arr = [
-        {id:1,'name':'blog 1'},
-        {id:2,'name':'blog 2'},
-        {id:3,'name':'blog 3'},
-    ];
-    const loading = false;
-    const showComments = true;
-    const comments = (
-        <div className="comments">
-        <h3>posts:({arr.length})</h3>
-        <ul>
-        {arr.map((post,index)=>
-            <li key={index}> {post.name}</li>
-            )}     
-        </ul>
-    </div>
-    )
-    if(loading) return (<h1>Loading...</h1>)
+function App(){
+ const [feedback,setFeedback] = useState(FeedbackData);
     return(
+        <>
+        <Header/>
         <div className="container">
-        <h1>{tittle.toUpperCase()}</h1>
-        <p>{para}</p>
-         {showComments && comments}
+        <FeedbackList feedback={feedback}/>
         </div>
+        </>
     )
 }
 
